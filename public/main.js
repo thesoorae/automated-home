a11f2b4d0ccedc2557ff91426d361711d595f630
The Soo-Rae Hong <the.soorae@gmail.com>
2017-03-04 14:28:42 -0800

    change root

--- public/main.js
+++ public/main.js
@@ -2,8 +2,8 @@ const House = require('./js/house');
 
 // let root = "http://localhost:3000";
 // let root = "https://github.com/thesoorae/automated-home";
-let root = process.env.PORT || 3000;
-
+// let root = process.env.PORT || 3000;
+let root = "https://automated-home.herokuapp.com/";
 //get canvas
 $(document).ready(function(){
   const canvas = $("#canvas")[0];
